3dd995ccc6d2f90e5096e78a6ec531518104a056
MaryamKhallala <Maryam.khallala@etu.fstm.ac.ma>
2022-10-24 20:26:13 +0100

    Gestion de recrutement avec Spring

---
+++ src/main/java/com/example/SpringMaryKH/SpringMaryKhApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMaryKH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaryKhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMaryKhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringMaryKH/SpringMaryKhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMaryKH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMaryKhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
